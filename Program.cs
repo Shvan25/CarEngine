1c937d2aaa5d956ba32e7e21678cbf49fa3824a2
Name <vbrfqlj@mail.ru>
2025-03-02 14:05:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Car<T1> where T1 : Engine
+{
+    public T1 Engine;
+    public virtual void ChangePart<T2> (T2 NewPart) where T2 : CarPart
+    {
+        
+    }
+}
+class Engine
+{
+
+}
+class ElectricEngine : Engine
+{
+
+}
+class GasEngine : Engine
+{
+
+}
+class CarPart
+{
+
+}
+class Battery : CarPart
+{
+
+}
+class Differential : CarPart
+{
+
+}
+class Wheel : CarPart
+{
+
+}
